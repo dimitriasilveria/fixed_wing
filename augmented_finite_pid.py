correcting quaternion

augmented_finite_pid.py
@@ -21,12 +21,12 @@ class PID_fixed_wing():
         self.fixed_wing.reset(state={"roll": -0.5, "pitch": 0.15})
         self.t_max = 1
         self.N = self.t_max*10 #number of points
-        self.T = (self.t_max-0)/self.N 
+        self.T = self.fixed_wing.dt
         self.T_p = 30 
         self.qsi = np.zeros((9,1)) 
         self.n_agents = 1
         self.mb = self.fixed_wing.params["mass"]
-        self.g = self.fixed_wing.g
+        self.g = -self.fixed_wing.g
         self.ga = np.array([0, 0, -self.g])
 
         self.Jb_Bz = np.diag([0.0112,0.01123,0.02108]) 
@@ -39,7 +39,7 @@ class PID_fixed_wing():
         self.t = np.linspace(0,self.t_max,self.N) #time
 
         #reference trajectories###################################################
-        self.r = 1 
+        self.r = 100 
         #circle
         # self.ra_r = np.vstack(self.r*np.cos(2*np.pi*self.t/self.T_p), self.r*np.sin(2*np.pi*self.t/self.T_p), [0.6*np.ones_like(self.t)])  #reference position
         # self.va_r = np.vstack((2*np.pi/self.T_p)*(-self.r*np.sin(2*np.pi*self.t/self.T_p)), [(2*np.pi/self.T_p)*self.r*np.cos(2*np.pi*self.t/self.T_p), 0.6*np.zeros_like(self.t)]) #reference linear velocity
@@ -47,7 +47,7 @@ class PID_fixed_wing():
         self.wr_r = np.zeros((3,self.N))  #reference angular velocity
         self.Car = np.zeros((3,3,self.N))  #reference attitude
         #self.Car[:,:,0] = np.eye(3) #eul2rotm([pi*0.5 0 0])*Ca_r(:,:,1) #
-        self.z_w = np.array([0,0,-1])
+        self.z_w = np.array([0,0,1])
         self.f_r= np.zeros((3,self.N))  #reference force
         # self.f_A_r = np.zeros((3,self.N))  #reference aerodynamics force
         #controller and state-space matrices ###################################
@@ -91,10 +91,12 @@ class PID_fixed_wing():
         self.fixed_wing.state["velocity_u"].value = self.va_r[0,0] 
         self.fixed_wing.state["velocity_v"].value = self.va_r[1,0] 
         self.fixed_wing.state["velocity_w"].value = self.va_r[2,0] 
+        
         self.d_v = self.Cab[:,:,0].T@(self.va_r[:,0] - self.X[3:6,0]) 
         self.d_r = self.Cab[:,:,0].T@(self.ra_r[:,0] - self.X[6:9,0]) 
         self.d_Xi[0:9,0] = dX_to_dXi(self.dC[:,:,0],self.d_v,self.d_r) 
-        
+
+       
         # self.d_Xi[9:12,0] = self.c1*self.d_Xi[6:9,0] + self.d_Xi[3:6,0] 
 
 
@@ -137,6 +139,7 @@ class PID_fixed_wing():
 
 
         self.Car[:,:,i] =  Rz @ Ry @ Rx
+        self.f_r[:,i] = self.Car[:,:,i]@(self.mb*self.va_r_dot[:,i] - self.mb*self.g*self.z_w)
         # norm_aux = np.linalg.norm(self.va_r_dot[:,i]-(self.va_r_dot[:,i]@self.va_r[:,i])*self.va_r[:,i]) 
         # if (norm_x_B != 0) and (norm_aux != 0):
         #     x_B = self.va_r[:,i]/norm_x_B
@@ -162,10 +165,8 @@ class PID_fixed_wing():
         # self.Car[:,:,i] = np.hstack((x_B.reshape(3,1), y_B.reshape(3,1), z_B.reshape(3,1)))#@R.from_euler('xyz', [np.pi/100, 0, 0]).as_matrix()
         if i > 0 :
             self.wr_r[:,i] = so3_R3(logm(self.Car[:,:,i-1].T@self.Car[:,:,i]))/self.T
-            self.f_r[:,i] = self.Car[:,:,i]@(self.mb*self.va_r_dot[:,i] - self.mb*self.g*self.z_w)
-            ic(self.f_r[:,i])
+            
             # if np.linalg.det(self.Car[:,:,i]) != 0 and np.linalg.det(self.Car[:,:,i+1]) != 0:
-
                 
             # else:
             #     self.wr_r[:,i] = np.zeros(3)
@@ -204,11 +205,12 @@ class PID_fixed_wing():
 
     def control(self):
         self.references(0)
+        self.references(0)
         attitude = R.from_matrix(self.Car[:,:,0]).as_quat()
+        self.fixed_wing.state["attitude"].set_value(attitude)
         omega = self.wr_r[:,0]
         vel = self.va_r[:,0]
-        controls = np.zeros(4)
-        # f = self.fixed_wing.reference_forces(attitude, omega, vel, controls)
+
 
         # self.f[:,0] = f
         for i in range(0,self.N-1):
@@ -218,14 +220,13 @@ class PID_fixed_wing():
             # ic(self.d_Xi[0:9,i])
             # input()
             self.dU[:,i] = -self.K_pid[:,:,i]@self.d_Xi[:,i]
-            #self.dU[0:3,i] =np.clip(self.dU[0:3,i],-1,1)
-            self.f[:,i] = self.f_r[:,i] - self.dU[0:3,i]
+            self.dU[0:3,i] =np.clip(self.dU[0:3,i],-1,1)
+            self.f[:,i] = self.f_r[:,i] #- self.dU[0:3,i]
             # self.f_r[:,i+1] = self.f[:,i]
-            self.wb_b_cont[:,i] = self.dC[:,:,i]@self.wr_r[:,i] - self.dU[3:6,i]
+            self.wb_b_cont[:,i] = self.dC[:,:,i]@self.wr_r[:,i] #- self.dU[3:6,i]
             #self.wb_b_cont[:,i] = np.clip(self.wb_b_cont[:,i],-np.pi/3,np.pi/3)
             attitude = R.from_matrix(self.Car[:,:,i]).as_quat()
             controls = self.fixed_wing.calc_control(attitude, self.va_r[:,i], self.wb_b_cont[:,i], self.f[:,i]) #should I use Cab or Car? 
-            ic(controls)
             _, _ = self.fixed_wing.step(controls)
             #self.f_r[:,i+1] = self.fixed_wing.reference_forces(attitude, omega, vel, controls)
             self.X[0,i+1] = self.fixed_wing.state["omega_p"].value
@@ -237,6 +238,10 @@ class PID_fixed_wing():
             self.X[3,i+1] =self.fixed_wing.state["velocity_u"].value
             self.X[4,i+1] =self.fixed_wing.state["velocity_v"].value
             self.X[5,i+1] =self.fixed_wing.state["velocity_w"].value
+            self.fixed_wing.state["attitude"].set_value(R.from_matrix(self.Car[:,:,i+1]).as_quat())
+            self.fixed_wing.state["omega_p"].value = self.wr_r[0,i+1]
+            self.fixed_wing.state["omega_q"].value = self.wr_r[1,i+1]
+            self.fixed_wing.state["omega_r"].value = self.wr_r[2,i+1]
             self.Cab[:,:,i+1] = R.from_quat(self.fixed_wing.state["attitude"].value).as_matrix()
             self.dC[:,:,i+1] = self.Cab[:,:,i+1].T@self.Car[:,:,i+1]
             self.d_v = self.Cab[:,:,i+1].T@(self.va_r[:,i+1] - self.X[3:6,i+1]) 

pyfly/pyfly/pyfly.py
@@ -481,20 +481,21 @@ class Actuation:
             dynamics_commands = {"elevon_right": elevon_r_c, "elevon_left": elevon_l_c}
 
         for state in self.dynamics:
+            
             if state in self.input_indices:
                 state_c = commands[self.input_indices[state]]
             else:  # Elevail inputs with elevon dynamics
                 state_c = dynamics_commands[state]
             self.states[state].set_command(state_c)
             dynamics_commands[state] = self.states[state].command
-
+            
         # The elevator and aileron commands constrained by limitatons on physical elevons
         if self.elevon_dynamics:
             elev_c, ail_c = self._map_elevon_to_elevail(er=dynamics_commands["elevon_right"],
                                                         el=dynamics_commands["elevon_left"])
             self.states["elevator"].set_command(elev_c)
             self.states["aileron"].set_command(ail_c)
-
+           
         for state, i in self.input_indices.items():
             commands[i] = self.states[state].command
 
@@ -891,7 +892,7 @@ class PyFly:
                 self.params = json.load(param_file)
         else:
             raise Exception("Unsupported parameter file extension.")
-        self.params["C_Y_delta_r"] = 0.0001
+        self.params["C_Y_delta_r"] = 0.01
 
         self.I = np.array([[self.params["Jx"], 0, -self.params["Jxz"]],
                            [0, self.params["Jy"], 0, ],
@@ -935,7 +936,6 @@ class PyFly:
 
         self.state["attitude"] = AttitudeQuaternion()
         self.attitude_states_with_constraints = []
-
         self.actuation = Actuation(model_inputs=self.model_inputs,
                                    actuator_inputs=self.cfg["actuation"]["inputs"],
                                    dynamics=self.cfg["actuation"]["dynamics"])
@@ -1079,7 +1079,6 @@ class PyFly:
 
         # Record command history and apply conditions on actuator setpoints
         control_inputs = self.actuation.set_and_constrain_commands(commands)
-
         y0 = list(self.state["attitude"].value)
         y0.extend(self.get_states_vector(["omega_p", "omega_q", "omega_r", "position_n", "position_e", "position_d",
                                           "velocity_u", "velocity_v", "velocity_w"]))
@@ -1221,8 +1220,7 @@ class PyFly:
         :return: ([float], [float]) forces and moments in x, y, z of aircraft frame
         """
         elevator, aileron, rudder, throttle = controls
-        #elevator, aileron, rudder, throttle = self.calc_control(attitude, vel, omega, f_prop_ref, f_aero_ref)
-        
+       
 
         p, q, r = omega
 
@@ -1238,7 +1236,7 @@ class PyFly:
 
         pre_fac = 0.5 * self.rho * Va ** 2 * self.params["S_wing"]
 
-        e0, e1, e2, e3 = attitude
+        e1, e2, e3, e0 = attitude
         fg_b = self.params["mass"] * self.g * np.array([2 * (e1 * e3 - e2 * e0),
                                                         2 * (e2 * e3 + e1 * e0),
                                                         e3 ** 2 + e0 ** 2 - e1 ** 2 - e2 ** 2])
@@ -1294,12 +1292,12 @@ class PyFly:
         tau_prop = np.array([-self.params["k_T_P"] * (self.params["k_Omega"] * throttle) ** 2, 0, 0])
 
         f = f_prop + fg_b + f_aero
-        ic(fg_b)
         tau = tau_aero + tau_prop
-
+        ic(f)
         return f, tau
     
     def calc_control(self, attitude, vel, omega, f):
+
         f_lift_s = f[2]
         f_y = f[1]
         f_prop_drag = f[0]
@@ -1336,17 +1334,18 @@ class PyFly:
         elevator = ((f_lift_s/pre_fac) - (C_L_alpha + self.params["C_L_q"] * self.params["c"] / (2 * Va) * q))/self.params["C_L_delta_e"]
 
         C_fy = (f_y/pre_fac) - (self.params["C_Y_0"] + self.params["C_Y_beta"] * beta + self.params["C_Y_p"] * self.params["b"] / (2 * Va) * p + self.params["C_Y_r"] * self.params["b"] / (2 * Va) * r)
-        C_n = - (self.params["C_n_0"] + self.params["C_n_beta"] * beta + self.params["C_n_p"] * self.params["b"] / (
-                2 * Va) * p + self.params["C_n_r"] * self.params["b"] / (2 * Va) * r )
+        C_n = - (self.params["C_n_0"] + self.params["C_n_beta"] * beta + self.params["C_n_p"] * self.params["b"] / (2 * Va) * p + self.params["C_n_r"] * self.params["b"] / (2 * Va) * r )
         rudder = (self.params["C_Y_delta_a"]*C_n - self.params["C_Y_delta_r"]*C_fy)/(self.params["C_Y_delta_a"]*self.params["C_n_delta_r"] - self.params["C_Y_delta_r"]*self.params["C_n_delta_a"])
         aileron = (C_fy - self.params["C_Y_delta_r"]*rudder)/self.params["C_Y_delta_a"]
-        f_drag_s = pre_fac * (
-                            C_D_alpha + C_D_beta + self.params["C_D_q"] * self.params["c"] / (2 * Va) * q + self.params[
-                        "C_D_delta_e"] * elevator ** 2)
+        f_drag_s = pre_fac * (C_D_alpha + C_D_beta + self.params["C_D_q"] * self.params["c"] / (2 * Va) * q + self.params["C_D_delta_e"] * elevator ** 2)
         f_prop = f_prop_drag + f_drag_s
 
         throttle = (1/self.params["k_motor"]) * np.sqrt(f_prop/(0.5 * self.rho * self.params["S_prop"] * self.params["C_prop"]) + Va**2)
 
+        aileron = np.clip(aileron,np.deg2rad(-30),np.deg2rad(30))
+        elevator = np.clip(elevator,np.deg2rad(-30),np.deg2rad(35))
+        rudder = np.clip(rudder,np.deg2rad(-30),np.deg2rad(30))
+        throttle = np.clip(throttle,0,1)
         return np.array([elevator, aileron, rudder, throttle])
 
     def reference_forces(self, attitude, omega, vel, controls):